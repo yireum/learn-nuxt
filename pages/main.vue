4704079b2ef63f8a11b906997dbb9b89a00c3e73
yoojinkim <lastepisode@itsnowball.com>
2022-10-13 19:01:55 +0900

    api호출까지

--- pages/main.vue
+++ pages/main.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
-    <Logo />
     <h1>메인페이지</h1>
   </div>
 </template>
 <script>
-import Logo from '@/components/NuxtLogo.vue'
+import axios from 'axios'
 export default {
-  components: { Logo },
+  async created() {
+    try {
+      const response = await axios.get('http://localhost:3000/products')
+      console.log('에러없다', response)
+    } catch (error) {
+      console.log(error)
+    }
+  },
 }
 </script>
 <style scoped></style>
